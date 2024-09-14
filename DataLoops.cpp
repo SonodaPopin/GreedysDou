a3121436120e182daaba8d96d41dc1e7b2556e6b
Hugo Flores <hflores2022@udec.cl>
2024-09-14 17:45:57 -0300

    loops

---
+++ DataLoops.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <chrono>
+#include <algorithm>
+#include "Greedy.cpp"
+#include "AGreedy.cpp"
+
+void loopGreedy(){
+
+}
+
+void loopAGreedy(){
+
+}
+
+int main() {
+    loopGreedy();
+    loopAGreedy();
+    return 0;
+}
